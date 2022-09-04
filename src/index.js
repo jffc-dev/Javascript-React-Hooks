dabbe91e1f96650074137ca2334cbf87c56c0c5e
Javier Flores <jffc.dev@gmail.com>
2022-09-04 18:54:19 -0500

    feat(useState): Object as state and destructure in state declaration example added

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {HooksApp} from './HooksApp'
+import { Counterpp } from './components/01-useState/Counterpp';
+// import {HooksApp} from './HooksApp'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HooksApp />
+    <Counterpp />
   </React.StrictMode>
 );
