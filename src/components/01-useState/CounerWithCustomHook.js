1f05c9f410bfd54bda0429534f7f3607107855b1
Javier Flores <jffc.dev@gmail.com>
2022-09-04 19:05:49 -0500

    feat(useState): Custom hook example added

---
+++ src/components/01-useState/CounerWithCustomHook.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { useCounter } from '../../hooks/useCounter'
+
+import './counter.css'
+
+export const CounteWithCustomHook = () => {
+
+    const {state, increment, decrement } = useCounter(541);
+
+    return (
+        <>
+            <h1>Counter with hook {state}</h1>
+            <hr/>
+
+            <button className='btn' onClick={increment}>+1</button>
+            <button className='btn' onClick={decrement}>-1</button>
+        </>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Counterpp } from './components/01-useState/Counterpp';
+import { CounteWithCustomHook } from './components/01-useState/CounerWithCustomHook';
+// import { Counterpp } from './components/01-useState/Counterpp';
 // import {HooksApp} from './HooksApp'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Counterpp />
+    <CounteWithCustomHook />
   </React.StrictMode>
 );
