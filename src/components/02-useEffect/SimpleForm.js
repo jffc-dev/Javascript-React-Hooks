650a879145c646474ca38e8c4895613f5c967bab
Javier Flores <jffc.dev@gmail.com>
2022-09-06 02:12:23 -0500

    feat(useEffect): UseEffect hook example added

---
+++ src/components/02-useEffect/SimpleForm.js
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from 'react'
+import './effects.css'
+
+export const SimpleForm = () => {
+
+    const [formState, setFormState] = useState({
+        name: '',
+        email: ''
+    })
+
+    const {name, email} = formState;
+ 
+    //throwed every time a state changes
+    // useEffect( ()=> {
+    //     console.log("hey");
+    // });
+
+    //throwed every time a state changes
+    useEffect( ()=> {
+        console.log("initial");
+    }, []);
+
+    useEffect( ()=> {
+        console.log("formState changed");
+    }, [formState]);
+
+    useEffect( ()=> {
+        console.log("name changed");
+    }, [name]);
+
+    const handleInputChange = ({target}) => {
+        setFormState({
+            ...formState,
+            [target.name]: target.value
+        })
+    }
+
+    return (
+        <>
+            <h1>UseEffect</h1>
+            <hr/>
+
+            <div className='form-group'>
+                <input type="text" name="name" className="form-control" placeholder="Your name"
+                        autoComplete='off' value={name} onChange={handleInputChange}/>
+            </div>
+
+            <div className='form-group'>
+                <input type="text" name="email" className="form-control" placeholder="Your email"
+                        autoComplete='off' value={email} onChange={handleInputChange}/>
+            </div>
+        </>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { CounteWithCustomHook } from './components/01-useState/CounerWithCustomHook';
+import { SimpleForm } from './components/02-useEffect/SimpleForm';
+// import { CounteWithCustomHook } from './components/01-useState/CounerWithCustomHook';
 // import { Counterpp } from './components/01-useState/Counterpp';
 // import {HooksApp} from './HooksApp'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <CounteWithCustomHook />
+    <SimpleForm />
   </React.StrictMode>
 );
