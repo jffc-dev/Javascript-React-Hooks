cf82954850f42990b6365ccca88e9d8f8846f923
Javier Flores <jffc.dev@gmail.com>
2022-09-06 02:38:55 -0500

    feat(useEffect): Example of precautions with effects (eventListener) added

--- src/components/02-useEffect/Message.js
+++ src/components/02-useEffect/Message.js
@@ -1,12 +1,23 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 export const Message = () => {
 
+    const [coords, setCoords] = useState({x:0 , y:0})
+    const {x, y} = coords
+
     useEffect(() => {
         console.log('Componente montado');
+
+        const mouseMove = (e)=>{
+            const coords = { x: e.x, y: e.y}
+            setCoords(coords)
+            console.log(coords);
+        }
+
+        window.addEventListener('mousemove',mouseMove)
         
         return () => {
-            console.log('Componente desmontado');
+            window.removeEventListener('mousemove',mouseMove)
         }
     }, [])
     
@@ -14,6 +25,8 @@ export const Message = () => {
     return (
         <div>
             <h3>You're great</h3>
+            <h2>Coords</h2>
+            <p>x: {x}, y: {y}</p>
         </div>
     )
 }
