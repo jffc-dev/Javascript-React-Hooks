7f0228a161a01b35b3d82a54b2cc7f1f52ce3652
Javier Flores <jffc.dev@gmail.com>
2022-09-09 00:37:55 -0500

    feat(custom-hook): Call and get info from breaking bad api and rendering in screen

--- src/components/03-examples/MultipleCustomHooks.js
+++ src/components/03-examples/MultipleCustomHooks.js
@@ -4,12 +4,31 @@ import '../02-useEffect/effects.css'
 
 export const MultipleCustomHooks = () => {
 
-    const state = useFetch(`https://breakingbadapi.com/api/quotes/1`)
-    console.log(state);
+    const { loading, data } = useFetch(`https://breakingbadapi.com/api/quotes/1`);
+    const { author, quote } = !!data && data[0];
 
     return (
         <div>
-            <h1>MultipleCustomHooks</h1>
+            <h1>BreakingBad Quotes</h1>
+            <hr/>
+
+            {
+                loading
+                ?
+                (
+                    <div className='alert alert-info text-center'>
+                        Loading...
+                    </div>
+                )
+                :
+                (
+                    <blockquote className='blockquote text-end'>
+                        <p className=''>{quote}</p>
+                        <footer className='blockquote-footer'>{author}</footer>
+                    </blockquote>
+                )
+            }
+            
         </div>
     )
 }
