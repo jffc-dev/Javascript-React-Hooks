8a3d277c37087d0a746fc0ccff6ac1507e7166b5
Javier Flores <jffc.dev@gmail.com>
2022-09-09 22:32:01 -0500

    feat(useRef): UseRef hook example added

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import { FormWithCUstomHook } from './components/02-useEffect/FormWithCustomHook';
-import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks';
+// import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks';
+import { FocusScreen } from './components/04-useRef/FocusScreen';
 // import { SimpleForm } from './components/02-useEffect/SimpleForm';
 // import { CounteWithCustomHook } from './components/01-useState/CounerWithCustomHook';
 // import { Counterpp } from './components/01-useState/Counterpp';
@@ -10,6 +11,6 @@ import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHook
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <MultipleCustomHooks />
+    <FocusScreen />
   </React.StrictMode>
 );
