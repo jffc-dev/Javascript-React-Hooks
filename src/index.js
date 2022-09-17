f729a5932bd4ad16644942b5a8e61118cce43a79
Javier Flores <jffc.dev@gmail.com>
2022-09-17 12:37:52 -0500

    feat(reducer): Intro to Reduce - example added

--- src/index.js
+++ src/index.js
@@ -1,22 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-// import { CallbackHook } from './components/06-memos/CallbackHook';
-import { Padre } from './components/07-tarea-memo/Padre';
-// import { MemoHook } from './components/06-memos/MemoHook';
-// import { FormWithCUstomHook } from './components/02-useEffect/FormWithCustomHook';
-// import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks';
-// import { FocusScreen } from './components/04-useRef/FocusScreen';
-// import { RealExampleRef } from './components/04-useRef/RealExampleRef';
-// import { Layout } from './components/05-useLayoutEffect/LayoutEffect';
-// import { Memorize } from './components/06-memos/Memorize';
-// import { SimpleForm } from './components/02-useEffect/SimpleForm';
-// import { CounteWithCustomHook } from './components/01-useState/CounerWithCustomHook';
-// import { Counterpp } from './components/01-useState/Counterpp';
-// import {HooksApp} from './HooksApp'
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// // import { CallbackHook } from './components/06-memos/CallbackHook';
+// import { Padre } from './components/07-tarea-memo/Padre';
+// // import { MemoHook } from './components/06-memos/MemoHook';
+// // import { FormWithCUstomHook } from './components/02-useEffect/FormWithCustomHook';
+// // import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks';
+// // import { FocusScreen } from './components/04-useRef/FocusScreen';
+// // import { RealExampleRef } from './components/04-useRef/RealExampleRef';
+// // import { Layout } from './components/05-useLayoutEffect/LayoutEffect';
+// // import { Memorize } from './components/06-memos/Memorize';
+// // import { SimpleForm } from './components/02-useEffect/SimpleForm';
+// // import { CounteWithCustomHook } from './components/01-useState/CounerWithCustomHook';
+// // import { Counterpp } from './components/01-useState/Counterpp';
+// // import {HooksApp} from './HooksApp'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <Padre/>
-  </React.StrictMode>
-);
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <Padre/>
+//   </React.StrictMode>
+// );
+
+import './components/08-useReducer/intro-reducer.js'
