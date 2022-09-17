f0a6cad614e560db10ffbecb11199a5405911381
Javier Flores <jffc.dev@gmail.com>
2022-09-17 13:06:16 -0500

    feat(todoApp): Initial design added

--- src/components/08-useReducer/TodoApp.js
+++ src/components/08-useReducer/TodoApp.js
@@ -6,7 +6,7 @@ export const TodoApp = () => {
 
     const initialState = [{
         id: new Date().getTime(),
-        todo: 'LearnReact',
+        desc: 'LearnReact',
         done: false
     }];
 
@@ -16,14 +16,43 @@ export const TodoApp = () => {
 
     return (
         <div>
-            <h1>TodoApp</h1>
+            <h1>TodoApp ({todos.length})</h1>
             <hr/>
 
-            <ul>
-                <li>Hello</li>
-                <li>World</li>
-                <li>Again</li>
-            </ul>
+            <div className='row'>
+
+                <div className='col-7'>
+                    <ul className='list-group list-group-flush'>
+                        {
+                            todos.map((todo, index)=>{
+                                return <li 
+                                key={todo.id}
+                                className='list-group-item'>
+                                    <p className='text-center'>{index+1}. {todo.desc}</p>
+                                    <button className='btn btn-danger'>Borrar</button>
+                                </li>
+                            })
+                        }
+                    </ul>
+                </div>
+                
+                <div className='col-5'>
+
+                    <h4>Add TODO</h4>
+                    <hr/>
+
+                    <form>
+                        <input type='text'
+                        name='description'
+                        placeholder='what are you going to do?'
+                        autoComplete='off'
+                        className='form-control'></input>
+
+                        <button className='btn btn-outline-primary mt-1 w-100'>Add</button>
+                    </form>
+                </div>
+
+            </div>
         </div>
     )
 }
