2542074d82ba60aff027fde4e2f6a3d1bf212578
Javier Flores <jffc.dev@gmail.com>
2022-09-19 22:41:53 -0500

    improvement(todoApp): App broken down into components

--- src/components/08-useReducer/TodoApp.js
+++ src/components/08-useReducer/TodoApp.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useReducer } from 'react'
 import { useForm } from '../../hooks/useForm';
 import './styles.css'
+import { TodoList } from './TodoList';
 import { todoReducer } from './todoReducer';
 
 export const TodoApp = () => {
@@ -72,19 +73,7 @@ export const TodoApp = () => {
             <div className='row'>
 
                 <div className='col-7'>
-                    <ul className='list-group list-group-flush'>
-                        {
-                            todos.map((todo, index)=>{
-                                return <li 
-                                key={todo.id}
-                                className='list-group-item'>
-                                    <p className={`${todo.done && 'complete'}`} onClick={()=>{handleToggle(todo.id)}}>
-                                        {index+1}. {todo.desc}</p>
-                                    <button className='btn btn-danger' onClick={()=>{handleDelete(todo.id)}}>Delete</button>
-                                </li>
-                            })
-                        }
-                    </ul>
+                    <TodoList todos={todos} handleToggle={handleToggle} handleDelete={handleDelete}/>
                 </div>
                 
                 <div className='col-5'>
