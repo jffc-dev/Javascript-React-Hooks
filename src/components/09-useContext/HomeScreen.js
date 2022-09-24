57bb0705f6c81af38679973fa4fad39a4d48e6f3
Javier Flores <jffc.dev@gmail.com>
2022-09-23 20:22:15 -0500

    feat(useContext): createContext function exampe added

--- src/components/09-useContext/HomeScreen.js
+++ src/components/09-useContext/HomeScreen.js
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { UserContext } from './UserContext'
 
 export const HomeScreen = () => {
-  return (
-    <div>
-        <h1>HomeScreen</h1>
-        <hr/>
-    </div>
-  )
+
+    const userContext = useContext(UserContext);
+
+    console.log(userContext);
+
+    return (
+        <div>
+            <h1>HomeScreen</h1>
+            <hr/>
+        </div>
+    )
 }
--- src/components/09-useContext/MainApp.js
+++ src/components/09-useContext/MainApp.js
@@ -1,10 +1,20 @@
 import React from 'react'
 import { AppRouter } from './AppRouter'
+import { UserContext } from './UserContext'
 
 export const MainApp = () => {
-  return (
-    <div>
-        <AppRouter></AppRouter>
-    </div>
-  )
+
+    const user = {
+        id: 1234,
+        name: 'Javier Flores',
+        email: 'jffc.dev@gmail.com'
+    }
+
+    return (
+        <div>
+            <UserContext.Provider value={user}>
+                <AppRouter></AppRouter>
+            </UserContext.Provider>
+        </div>
+    )
 }
