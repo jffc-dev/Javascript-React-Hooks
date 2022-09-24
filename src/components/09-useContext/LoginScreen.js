cafad71ea0d08ceacfb2f5867b7050665c27c0bf
Javier Flores <jffc.dev@gmail.com>
2022-09-23 21:56:21 -0500

    feat(useContext): UseContext used to communicate components

--- src/components/09-useContext/LoginScreen.js
+++ src/components/09-useContext/LoginScreen.js
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { UserContext } from './UserContext';
 
 export const LoginScreen = () => {
-  return (
-    <div>
-        <h1>LoginScreen</h1>
-        <hr/>
-    </div>
-  )
+
+    const {setUser} = useContext(UserContext);
+
+    return (
+        <div>
+            <h1>LoginScreen</h1>
+            <hr/>
+            <button
+                className='btn btn-primary'
+                onClick={()=>{setUser({
+                    id: 1,
+                    name: 'Javier'
+                })}}
+            >Login</button>
+        </div>
+    )
 }
--- src/components/09-useContext/MainApp.js
+++ src/components/09-useContext/MainApp.js
@@ -1,18 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { AppRouter } from './AppRouter'
 import { UserContext } from './UserContext'
 
 export const MainApp = () => {
 
-    const user = {
-        id: 1234,
-        name: 'Javier Flores',
-        email: 'jffc.dev@gmail.com'
-    }
+    // const user = {
+    //     id: 1234,
+    //     name: 'Javier Flores',
+    //     email: 'jffc.dev@gmail.com'
+    // }
+
+    const [user, setUser] = useState({});
 
     return (
         <div>
-            <UserContext.Provider value={user}>
+            <UserContext.Provider value={{
+                user, setUser
+            }}>
                 <AppRouter></AppRouter>
             </UserContext.Provider>
         </div>
